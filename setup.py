36cde4b7b07ca79436649bf6389296ef4eb62ca7
Nemoleoliu <nemo.leo.liu@gmail.com>
2017-01-20 22:36:57 +0800

    Fix small bugs

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ from data_models import *
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Setup db.')
-    parser.add_argument('--dropfirst', dest='dropfirst',action='store_const', const=True, default=False,
-                        help = 'WARNING!!!This will drop all table first and recreate them.')
-    parser.add_argument('--fakedata', dest='fakedata',action='store_const', const=True, default=False,
-                        help = 'Fake some data.')
+    parser.add_argument('--dropfirst', dest='dropfirst', action='store_const', const=True, default=False,
+                        help='WARNING!!!This will drop all table first and recreate them.')
     parser.add_argument('-d', dest='debug', action='store_const', const=True, default=False,
                         help='Debug mode enabled.')
     args = parser.parse_args()
